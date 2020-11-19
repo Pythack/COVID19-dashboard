f4f8859b6ef55654c41f24c388bc7d4ab39ea0f4
Pythack <59161870+Pythack@users.noreply.github.com>
2020-11-19 11:06:26 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,40 +1,38 @@
-function chart_get(url, callback) {
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-    //alert('responseText: ' + xmlhttp.responseText);
-    try {
-        var response = xmlhttp.status;
-        var astro = JSON.parse(xmlhttp.responseText);
-        //var headers = xmlhttp.getAllResponseHeaders().toLowerCase();
-        //alert(headers['X-Rate-Limit-Remaining']);
-    } catch(err) {
-        console.warn(err.message + " in " + xmlhttp.responseText);
-        return;
-    }
-    callback(astro, response);
-}
-};
-
-xmlhttp.open("GET", url, true);
-xmlhttp.send();
-}
-
-
-var addtochart = document.getElementById('addtochart');
-addtochart.addEventListener('click', function() {
-  country = document.getElementById('country').value;
-  country.replace(' ', '-');
-  chart_get('https://api.covid19api.com/country/' + country + '/status/confirmed', function(data) {
-    var data = [
-  {
-    x: ['giraffes', 'orangutans', 'monkeys'],
-    y: [20, 14, 23],
-    type: 'bar'
+document.addEventListener("DOMContentLoaded", function() {
+  function chart_get(url, callback) {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+  if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+      //alert('responseText: ' + xmlhttp.responseText);
+      try {
+          var response = xmlhttp.status;
+          var astro = JSON.parse(xmlhttp.responseText);
+          //var headers = xmlhttp.getAllResponseHeaders().toLowerCase();
+          //alert(headers['X-Rate-Limit-Remaining']);
+      } catch(err) {
+          console.warn(err.message + " in " + xmlhttp.responseText);
+          return;
+      }
+      callback(astro, response);
   }
-];
+  };
 
-  Plotly.newPlot('myDiv', data);
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send();
+  }
 
+
+  var addtochart = document.getElementById('addtochart');
+  addtochart.addEventListener('click', function() {
+    country = document.getElementById('country').value;
+    country.replace(' ', '-');
+    chart_get('https://api.covid19api.com/country/' + country + '/status/confirmed', function(data) {
+      var chart_data = [{
+      x: [data[0]['country']],
+      y: [data[data.length - 1]['cases']],
+      type: 'bar'
+      }];
+      Plotly.newPlot('country_chart', chart_data);
+    });
   });
 });
